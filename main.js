e9c7ea241d85f62fa2767288219ff9a195c5a3a1
Maryam ALtawil <maryamraedaltawil@gmail.com>
2023-08-27 12:45:07 +0300

    completed courses

--- main.js
+++ main.js
@@ -10,17 +10,23 @@ let updateB = document.getElementById('updateB');
 let sortName = document.getElementById("sortName");
 let sortNameDown = document.getElementById("sortNameDown");
 let withoutSort = document.getElementById("withoutSort");
-
+let completedBtn =document.getElementById("completedBtn");
+let UncompletedBtn =document.getElementById("UncompletedBtn");
+let AllcoursesBtn =document.getElementById("AllcoursesBtn");
+let values = [];
+UncompletedBtn.style.display="none";
 updateB.style.display="none" 
+let coursesCompleted = [];
+let coursesUnCompleted = [];
 let courses =[];
+
 if (localStorage.getItem('courses')){
-    console.log('here')
     courses = JSON.parse(localStorage.getItem('courses'))
 }
 else {
  courses = []; 
 }
-display();
+display(courses);
 var btnAdd= document.getElementById('addBtn');
 
 btnAdd.onclick = function(e) {
@@ -30,37 +36,39 @@ btnAdd.onclick = function(e) {
         name: courseName.value ,
         category : courseCategory.value ,
         price  : coursePrice.value   ,
-        description    : courseDescription.value
+        description    : courseDescription.value ,
+        IsCompleted : false 
     }
     console.log(courses)
     courses.push(course);
     clear();
-    display()
+    display(courses)
 localStorage.setItem('courses',JSON.stringify(courses))
 }
 
-function display() {
-    var data = "";
+// function display( ) {
+//     var data = "";
     
-    for (var i = 0 ; i < courses.length ; i++) {
-        console.log(i);
-        data += `
-     <tr>
-        <td>${i + 1}</td>
-        <td>${courses[i].name}</td>
-        <td> </td>
-        <td> </td>
-        <td> </td>
-        <td>${courses[i].category}</td>
-        <td>${courses[i].price}</td>
-        <td>${courses[i].description}</td>
-        <td><button class="update-button" onclick="updateitem(${i})">Update</button></td>
-        <td><button class="delete-button" onclick="deleteitem(${i})">Delete</button></td>
-     </tr>`;
-    }
+//     for (var i = 0 ; i < courses.length ; i++) {
+//         console.log(i);
+//         data += `
+//      <tr>
+//         <td>${i + 1}</td>
+//         <td id="test${i}"> <input type="checkbox"  id="course${i}" name="course${i}" value="course${i}" class="checkbox"> </td>
+//         <td>${courses[i].name}</td>
+//         <td> </td>
+//         <td> </td>
+//         <td> </td>
+//         <td>${courses[i].category}</td>
+//         <td>${courses[i].price}</td>
+//         <td>${courses[i].description}</td>
+//         <td><button class="update-button" onclick="updateitem(${i})">Update</button></td>
+//         <td><button class="delete-button" onclick="deleteitem(${i})">Delete</button></td>
+//      </tr>`;
+//     }
 
-    tableBody.innerHTML = data;
-}
+//     tableBody.innerHTML = data;
+// }
 
 function clear() {
     courseName.value ="";
@@ -73,13 +81,13 @@ function clear() {
 function deleteitem(i){
     courses.splice(i, 1)
     localStorage.setItem('courses',JSON.stringify(courses))
-    display();
+    display(courses);
 }
    
 deleteBtn.onclick = function(){
     courses = [];
     localStorage.removeItem('courses')
-    display()
+    display(courses)
 }
 
 function updateitem(i){
@@ -100,7 +108,7 @@ function updateitem(i){
         }
         
         courses[i]=course;
-        display();
+        display(courses);
         clear();
         updateB.style.display="none";
         btnAdd.style.display="inline-block";
@@ -129,12 +137,12 @@ for (var i = 0 ; i < courses.length ; i++) {
 
    }
 }
-
+//order items alphabetically by name 
 sortNameDown.style.display="none";
 withoutSort.style.display="none";
 
 sortName.onclick = function(){
-    sortElement = courses.slice();
+   sortElement = courses.slice();
    courses.sort(function (a, b) {
         if (a.name < b.name) {
           return -1;
@@ -145,8 +153,8 @@ sortName.onclick = function(){
         return 0;
       });
       
-      console.log(courses); 
-      display();
+      
+      display(courses);
       sortName.style.display="none";
       sortNameDown.style.display="inline-block";
 
@@ -165,7 +173,7 @@ sortNameDown.onclick = function(){
       });
       
       console.log(courses); 
-      display();
+      display(courses);
 
       sortNameDown.style.display= "none";
 
@@ -175,13 +183,122 @@ sortNameDown.onclick = function(){
 
 withoutSort.onclick = function(){  
      courses=sortElement ;
-      display();
+      display(courses);
       sortName.style.display="inline-block";
       withoutSort.style.display= "none";
 }
 
 
 
+
+//add button completed with checkbox that will show completed
+//uncompleted courses based on its status
+
+
+AllcoursesBtn.onclick =function (){
+   display (courses); 
+}
+
+function completed(i){
+
+    let checkBox = document.getElementById(`course${i}`)
+    if (checkBox && checkBox.checked ){
+
+         courses[i].IsCompleted=true;
+         localStorage.setItem('courses',JSON.stringify(courses))
+
+    }
+    else{
+        courses[i].IsCompleted=false;
+        localStorage.setItem('courses',JSON.stringify(courses))
+
+    }
+}
+
+function completed(i, prefix) {
+    let checkBox = document.getElementById(`${prefix}${i}`);
+    
+    if (checkBox && checkBox.checked) {
+        courses[i].IsCompleted = true;
+        localStorage.setItem('courses', JSON.stringify(courses));
+    } else {
+        courses[i].IsCompleted = false;
+        localStorage.setItem('courses', JSON.stringify(courses));
+    }
+}
+
+
+
+
+completedBtn.onclick = function() {
+    let coursesCompleted = courses.filter(course => course.IsCompleted);
+    display(coursesCompleted, 'completedCourse');
+    completedBtn.style.display = "none";
+    UncompletedBtn.style.display = "inline-block";
+};
+
+
+UncompletedBtn.onclick = function(){
+    let uncoursesCompleted = [];
+    for (let i = 0; i < courses.length; i++) {
+        if (courses[i].IsCompleted==false) {
+            uncoursesCompleted.push(courses[i]);
+        }
+        
+    }
+ display(uncoursesCompleted, 'uncourse');    // display(arrayDiff(courses, coursesCompleted));
+         UncompletedBtn.style.display="none";
+         completedBtn.style.display ="inline-block";
+}
+
+
+function display(courseList, prefix) {
+    var data = "";
+
+    for (var i = 0; i < courseList.length; i++) {
+        data += `
+     <tr>
+        <td>${i + 1}</td>
+        <td id="test${i}"><input type="checkbox" id="${prefix}${i}" name="${prefix}${i}" class="checkbox" onclick="completed(${i}, '${prefix}')"> </td>
+        <td>${courseList[i].name}</td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td>${courseList[i].category}</td>
+        <td>${courseList[i].price}</td>
+        <td>${courseList[i].description}</td>
+        <td><button class="update-button" onclick="updateitem(${i})">Update</button></td>
+        <td><button class="delete-button" onclick="deleteitem(${i})">Delete</button></td>
+     </tr>`;
+    }
+
+    tableBody.innerHTML = data;
+
+    for (var i = 0; i < courseList.length; i++) {
+        let test = document.getElementById(`${prefix}${i}`);
+        if (courseList[i].IsCompleted) {
+            test.checked = true;
+        } else {
+            test.checked = false;
+        }
+    }
+
+
+        for (var i = 0; i < courseList.length; i++) {
+            if (prefix != undefined ){
+            let testNone = document.getElementById(`test${i}`);
+            testNone.style.display="none";
+            let d_none = document.getElementById("d_none");
+            d_none.style.display="none";
+            }
+            else{
+                d_none.style.display="inline-block";
+
+            }
+        }
+        
+
+    }
 
 
 
