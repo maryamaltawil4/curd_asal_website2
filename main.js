ffa78262539e36d624480163d79083ecc3d3e2ea
Maryam ALtawil <maryamraedaltawil@gmail.com>
2023-08-30 02:53:11 +0300

    update

--- main.js
+++ main.js
@@ -7,15 +7,14 @@ let buttons = document.getElementById("buttons");
 let searchBtn = document.getElementById("searchbtn");
 let UncompletedBtn_click = document.getElementById("UncompletedBtn");
 let completedBtn_click = document.getElementById("completedBtn");
-let withoutSortBtn= document.getElementById("withoutSort");
+let withoutSortBtn = document.getElementById("withoutSort");
 let updateB = document.getElementById("updateBtn");
 let btnAdd = document.getElementById("addBtn");
 let values = [];
 
 UncompletedBtn_click.style.display = "none";
 updateB.style.display = "none";
-let coursesCompleted = [];
-let coursesUnCompleted = [];
+
 let courses = [];
 
 if (localStorage.getItem("courses")) {
@@ -25,22 +24,21 @@ if (localStorage.getItem("courses")) {
 }
 display(courses);
 
-
- function AddClick (e) {
+function addClick(e) {
   e.preventDefault();
   var course = {
     name: courseName.value,
     category: courseCategory.value,
     price: coursePrice.value,
     description: courseDescription.value,
-    IsCompleted: false,
+    isCompleted: false,
   };
- 
+
   courses.push(course);
   clear();
   display(courses);
   localStorage.setItem("courses", JSON.stringify(courses));
-};
+}
 
 function clear() {
   courseName.value = "";
@@ -48,6 +46,7 @@ function clear() {
   coursePrice.value = " ";
   courseDescription.value = "";
 }
+
 // delete button
 function deleteitem(i) {
   courses.splice(i, 1);
@@ -55,11 +54,11 @@ function deleteitem(i) {
   display(courses);
 }
 
-function deleteBtn () {
+function deleteBtn() {
   courses = [];
   localStorage.removeItem("courses");
   display(courses);
-};
+}
 
 function updateitem(i) {
   const { name, category, price, description } = courses[i]; // object destructure
@@ -67,20 +66,18 @@ function updateitem(i) {
   courseCategory.value = category;
   coursePrice.value = price;
   courseDescription.value = description;
-  
 
   btnAdd.style.display = "none";
   updateB.style.display = "inline-block";
 
-  updateB.onclick = function(){
+  updateB.onclick = function () {
     var course = {
-        name: courseName.value ,
-        category : courseCategory.value ,
-        price  : coursePrice.value   ,
-        description    : courseDescription.value,
-        IsCompleted: courses[i].IsCompleted
-    
-    }
+      name: courseName.value,
+      category: courseCategory.value,
+      price: coursePrice.value,
+      description: courseDescription.value,
+      isCompleted: courses[i].isCompleted,
+    };
 
     courses[i] = course;
     display(courses);
@@ -89,123 +86,114 @@ function updateitem(i) {
     btnAdd.style.display = "inline-block";
   };
 }
-searchBtn.onkeyup = function () {
-  let key = searchBtn.value;
-  let data = "";
-  for (var i = 0; i < courses.length; i++) {
-    if (courses[i].name.toLowerCase().includes(key.toLowerCase())) {
-      data += `
-    <tr>
-       <td>${i + 1}</td>
-       <td>${courses[i].name}</td>
-       <td>${courses[i].category}</td>
-       <td>${courses[i].price}</td>
-       <td>${courses[i].description}</td>
-       <td><button class="update-button" onclick="updateitem(${i})">Update</button></td>
-       <td><button class="delete-button" onclick="deleteitem(${i})">Delete</button></td>
-    </tr>`;
-    }
-
-    tableBody.innerHTML = data;
-  }
-};
-
 //add button completed with checkbox that will show completed
 //uncompleted courses based on its status
 
-function AllcoursesBtn () {
+function AllcoursesBtn() {
   display(courses);
-};
+}
 
 function completed(i) {
-  let checkBox = document.getElementById(`${i}`); 
+  let checkBox = document.getElementById(`${i}`);
 
-  courses[i].IsCompleted = checkBox.checked;
-  localStorage.setItem("courses", JSON.stringify(courses)); 
+  courses[i].isCompleted = checkBox.checked;
+  localStorage.setItem("courses", JSON.stringify(courses));
 }
 
-sortNameDown.style.display="none";
-withoutSortBtn.style.display="none";
-sortName.style.display= "inline-block" ;
+sortNameDown.style.display = "none";
+withoutSortBtn.style.display = "none";
+sortName.style.display = "inline-block";
 
 function toggleSortOrder() {
- 
+  if (sortName.style.display == "inline-block") {
+    sortElement = courses.slice();
+    courses.sort(function (a, b) {
+      return a.name.localeCompare(b.name);
+    });
+    sortName.style.display = "none";
+    sortNameDown.style.display = "inline-block";
+  } else {
+    courses.sort(function (a, b) {
+      return b.name.localeCompare(a.name);
+    });
+    sortNameDown.style.display = "none";
+    withoutSortBtn.style.display = "inline-block";
+  }
 
-    if (sortName.style.display == "inline-block") {
-        sortElement = courses.slice();
-        courses.sort(function(a, b) {
-            return a.name.localeCompare(b.name);
-        });
-        sortName.style.display = "none";
-        sortNameDown.style.display = "inline-block";
-    } else {
-        courses.sort(function(a, b) {
-            return b.name.localeCompare(a.name);
-        });
-        sortNameDown.style.display = "none";
-        withoutSortBtn.style.display="inline-block";
-
-    }
-
-    display(courses);
+  display(courses);
 }
-// to display course without sort 
- function withoutSort (){  
-    courses=sortElement ;
-     display();
-     sortName.style.display="inline-block";
-     withoutSortBtn.style.display= "none";
- }
- function completedBtn () {
-  let coursesCompleted = courses.filter((course) => course.IsCompleted);
+// to display course without sort
+function withoutSort() {
+  courses = sortElement;
+  display(courses);
+  sortName.style.display = "inline-block";
+  withoutSortBtn.style.display = "none";
+}
+function completedBtn() {
+  let coursesCompleted = courses.filter((course) => course.isCompleted);
   display(coursesCompleted);
   completedBtn_click.style.display = "none";
   UncompletedBtn_click.style.display = "inline-block";
-};
+}
 
 function UncompletedBtn() {
   let uncoursesCompleted = [];
   for (let i = 0; i < courses.length; i++) {
-    if (courses[i].IsCompleted == false) {
+    if (courses[i].isCompleted == false) {
       uncoursesCompleted.push(courses[i]);
     }
   }
   display(uncoursesCompleted);
   UncompletedBtn_click.style.display = "none";
   completedBtn_click.style.display = "inline-block";
-};
+}
 
+// Function to generate HTML for a table row
+function generateTableRow(index, course, isChecked) {
+  return `
+    <tr>
+       <td>${index + 1}</td>
+       <td id="test${index}">
+       <input type="checkbox" id="${index}" ${isChecked} name="${index}" class="checkbox" onclick="completed(${index})"> </td>
+       <td>${course.name}</td>
+       <td> </td>
+       <td> </td>
+       <td> </td>
+       <td>${course.category}</td>
+       <td>${course.price}</td>
+       <td>${course.description}</td>
+       <td><button class="update-button" onclick="updateitem(${index})">Update</button></td>
+       <td><button class="delete-button" onclick="deleteitem(${index})">Delete</button></td>
+    </tr>`;
+}
+
+// Function to display course data
 function display(courseList) {
-  var data = "";
+  let data = "";
 
-  for (var i = 0; i < courseList.length; i++) {
-        //this line for completed /uncompleted course to have a same id for courses 
-    let index = courses.indexOf(courseList[i]);
-    data += `
-     <tr>
-        <td>${i + 1}</td>
-        <td id="test${i}"><input type="checkbox" id="${index}" name="${index}" class="checkbox" onclick="completed(${index})"> </td>
-        <td>${courseList[i].name}</td>
-        <td> </td>
-        <td> </td>
-        <td> </td>
-        <td>${courseList[i].category}</td>
-        <td>${courseList[i].price}</td>
-        <td>${courseList[i].description}</td>
-        <td><button class="update-button" onclick="updateitem(${index})">Update</button></td>
-        <td><button class="delete-button" onclick="deleteitem(${index})">Delete</button></td>
-     </tr>`;
+  for (let i = 0; i < courseList.length; i++) {
+    const course = courseList[i];
+    const index = courses.indexOf(course);
+    const isChecked = course.isCompleted ? "checked" : "";
+
+    data += generateTableRow(index, course, isChecked);
   }
 
   tableBody.innerHTML = data;
+}
 
-  for (var i = 0; i < courseList.length; i++) {
-    let index = courses.indexOf(courseList[i]); 
-    let test = document.getElementById(`${index}`); 
-    if (courseList[i].IsCompleted) {
-      test.checked = true;
-    } else {
-      test.checked = false;
+searchBtn.onkeyup = function () {
+  const key = searchBtn.value;
+  let data = "";
+
+  for (let i = 0; i < courses.length; i++) {
+    const course = courses[i];
+
+    if (course.name.toLowerCase().includes(key.toLowerCase())) {
+      const isChecked = course.isCompleted ? "checked" : "";
+      data += generateTableRow(i, course, isChecked);
     }
   }
-}
+
+  tableBody.innerHTML = data;
+};
